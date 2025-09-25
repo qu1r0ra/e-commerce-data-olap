d2a8debe679ebcae567a0cc1a2430f6f0e51e64c
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-09-25 23:14:37 +0800

    chore: refactor and add db files

---
+++ db/sql/inspect-tables.sql
@@ -0,0 +1,19 @@
+# Inspect the tables
+
+SELECT		*
+FROM		Couriers;
+
+SELECT		*
+FROM		OrderItems;
+
+SELECT		*
+FROM		Orders;
+
+SELECT		*
+FROM		Products;
+
+SELECT		*
+FROM		Riders;
+
+SELECT		*
+FROM		Users;
